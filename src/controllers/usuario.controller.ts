9fd14e3c73e719bcc2f9c0c9f14ece11ce15705e
Esteban Ochoa <esteban.ochoa28488@ucaldas.edu.co>
2023-09-10 22:56:47 -0500

    Se creo el segundo factor de verificacion

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -16,9 +16,10 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
-import {Usuario} from '../models';
-import {UsuarioRepository} from '../repositories';
+import {Credenciales, Login, Usuario} from '../models';
+import {LoginRepository, UsuarioRepository} from '../repositories';
 import {service} from '@loopback/core';
 import {SeguridadUsuarioService} from '../services';
 
@@ -27,7 +28,9 @@ export class UsuarioController {
     @repository(UsuarioRepository)
     public usuarioRepository : UsuarioRepository,
     @service(SeguridadUsuarioService)
-    public servicioSeguridad: SeguridadUsuarioService
+    public servicioSeguridad: SeguridadUsuarioService,
+    @repository(LoginRepository)
+    public repositorioLogin: LoginRepository
   ) {}
 
   @post('/usuario')
@@ -49,7 +52,7 @@ export class UsuarioController {
     usuario: Omit<Usuario, '_id'>,
   ): Promise<Usuario> {
     // crear la clave
-    let clave = this.servicioSeguridad.crearClave();
+    let clave = this.servicioSeguridad.crearTextoAleatorio(10);
     console.log(clave)
     // cifrar la clave
     let claveCifrada = this.servicioSeguridad.cifrarTexto(clave);
@@ -159,4 +162,41 @@ export class UsuarioController {
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.usuarioRepository.deleteById(id);
   }
+
+  /**
+   * Metodos personalizados para la API
+   */
+
+@post('/identificar-usuario')
+@response(200, {
+  description:"Identificar un usuario por correo y clave",
+  content:{'application/json':{schema: getModelSchemaRef(Credenciales)}}
+})
+async identificarUsuario(
+  @requestBody(
+    {
+      content:{
+        'applicacion/json':{
+          schema:getModelSchemaRef(Credenciales)
+        }
+      }
+    }
+  )
+  credenciales: Credenciales
+): Promise<object> {
+  let usuario = await this.servicioSeguridad.identificarUsuario(credenciales);
+  if (usuario){
+    let codigo2fa = this.servicioSeguridad.crearTextoAleatorio(5);
+    let login:Login = new Login();
+    login.usuarioId = usuario._id!;
+    login.codigo2fa = codigo2fa;
+    login.estadoCodigo2fa = false;
+    login.token="";
+    login.estadoToken = false;
+    this.repositorioLogin.create(login);
+    // Notificar al usuario via sms o correo
+    return usuario;
+  }
+  return new HttpErrors[401]("Credenciales Incorrectas")
+}
 }
--- src/services/seguridad-usuario.service.ts
+++ src/services/seguridad-usuario.service.ts
@@ -1,24 +1,52 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import {Credenciales, Usuario} from '../models';
+import {repository} from '@loopback/repository';
+import {UsuarioRepository} from '../repositories';
 const generator = require('generate-password');
 const MD5 = require("crypto-js/md5");
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class SeguridadUsuarioService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    @repository(UsuarioRepository)
+    public repositorioUsuario: UsuarioRepository
+  ) {}
 
-  /*
-   * Add service methods here
+  /**
+   * Crear una clave aleatoria
+   * @returns cadena aleatoria de n caracteres
    */
-  crearClave(): string{
+  crearTextoAleatorio(n:number): string{
     let clave = generator.generate({
-      length: 10,
+      length: n,
       numbers: true
     });
     return clave;
   }
 
+  /**
+   * Cifrar una cadena con metodo MD5
+   * @param cadena texto a cifrar
+   * @returns cadena cifrada con MD5
+   */
   cifrarTexto(cadena:string){
     let cadenaCifrada = MD5(cadena).toString();
     return cadenaCifrada
+  }
+
+  /**
+   * Se busca un usuario por sus credenciales de acceso
+   * @param credenciales credenciales de usuario
+   * @returns usuario encontrado o null
+   */
+
+  async identificarUsuario(credenciales: Credenciales): Promise<Usuario | null> {
+    let usuario = await this.repositorioUsuario.findOne({
+      where:{
+        correo: credenciales.correo,
+        clave: credenciales.clave
+      }
+    });
+    return usuario as Usuario
   }
 }
